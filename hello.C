8b45999aa3c0886657000f16f27e32bc2f65a9a0
Ronak Buch <ronakbuch@gmail.com>
2021-01-19 22:07:14 -0500

    Use regular index

--- hello.C
+++ hello.C
@@ -1,40 +1,6 @@
 #include <stdio.h>
 #include <chrono>
 #include <thread>
-// *MUST* include "charm++.h" to inherit from CkArrayIndex
-#include "charm++.h"
-
-// *MUST* declare index type *BEFORE* we include hello.decl.h!
-
-// This is our application-specific index type. It's a completely ordinary C++
-// class (or even C struct) -- the only requirement is that all the data be
-// allocated locally (no pointers, no virtual methods).
-class Fancy {
-private:
-  int value;
-//  int dummy;
-public:
-  Fancy() : value(0) {} //, dummy(0) {}
-  Fancy(int val) : value(val) {} //, dummy(0) {}
-  int getValue() const { return value; }
-};
-
-// This adapts the application's index for use by the array manager. This class
-// is only used by the translator -- you never need to refer to it again!
-class CkArrayIndexFancy : public CkArrayIndex {
-private:
-  Fancy* idx;
-public:
-  CkArrayIndexFancy() {
-    idx = new(index) Fancy();
-    nInts = sizeof(Fancy) / sizeof(int);
-  }
-
-  CkArrayIndexFancy(const Fancy& f) {
-    idx = new(index) Fancy(f);
-    nInts = sizeof(Fancy) / sizeof(int);
-  }
-};
 
 #include "hello.decl.h"
 
@@ -59,12 +25,11 @@ public:
     mainProxy = thisProxy;
     CProxy_Hello arr = CProxy_Hello::ckNew();
     for (int i = 0; i < nElements; i++) {
-      const Fancy f(i);
-      arr[f].insert();
+      arr[i].insert();
     }
     arr.doneInserting();
 
-    arr[Fancy(0)].SayHi();
+    arr[0].SayHi();
   };
 
   void done(void) {
@@ -73,37 +38,43 @@ public:
   };
 };
 
-/*array [Fancy]*/
 class Hello : public CBase_Hello {
 private:
   int counter;
+  int originalPE;
 public:
   Hello() {
-    // Note how thisIndex is of type fancyIndex:
     CkPrintf("Hello (%d) created on %d\n",
-             thisIndex.getValue(), CkMyPe());
+             thisIndex, CkMyPe());
     counter = 0;
+    originalPE = CkMyPe();
     usesAtSync = true;
   }
 
   Hello(CkMigrateMessage *m) {}
 
   void ResumeFromSync() {
-    CkPrintf("[%d] %d resume from sync\n", CkMyPe(), thisIndex.getValue());             
-    if (thisIndex.getValue() == 0)
+    CkPrintf("[%d] %d resume from sync, iter %d\n", CkMyPe(), thisIndex, counter);
+    if (thisIndex == 0)
       thisProxy[thisIndex].SayHi();
   }
 
   void pup(PUP::er &p) {
     p | counter;
+    p | originalPE;
   }
 
   void SayHi() {
     counter++;
-    const int value = thisIndex.getValue();
-    CkPrintf("[%d] Iter %d from element %d\n", CkMyPe(), counter, value);
+    const int value = thisIndex;
+    const int expectedPE = (originalPE + counter - 1) % CkNumPes();
+    CkPrintf("[%d](%d) *** Iter %d from element %d\n", CkMyPe(), expectedPE, counter, value);
+    //CkAssert(expectedPE == CkMyPe());
     if (value + 1 < nElements)
-      thisProxy[Fancy(thisIndex.getValue() + 1)].SayHi();
+    {
+      CkPrintf("[%d](%d) Sending to %d from element %d\n", CkMyPe(), expectedPE, thisIndex + 1, thisIndex);
+      thisProxy[thisIndex + 1].SayHi();
+    }
 
     //std::this_thread::sleep_for (std::chrono::milliseconds(1000));
     if (counter < iters) {
